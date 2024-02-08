4c148e960927a0f70a249208fcff5a23c8982069
dimonstools <dimonstools@googlemail.com>
2024-02-08 17:58:23 +0200

    Changed author and comparing two book lists

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -1,6 +1,7 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import java.util.ArrayList;
@@ -15,6 +16,25 @@ public class Book {
         PageFactory.initElements(driver, this);
     }
 
+//    @FindBy(xpath = "\"//a/i[@class=\\\"a-icon a-icon-addon p13n-best-seller-badge\\\"]\"")
+//    private static WebElement bestSeller;
+//
+//    @FindBy(xpath = "//span[@class=\"author notFaded\"]/..")
+//    private static WebElement author;
+//
+//    @FindBy(xpath = "//span[@id=\"productTitle\"]")
+//    private static WebElement title;
+//
+//    @FindBy(xpath = "//div[@id=\"rpi-attribute-book_details-publication_date\"]/div[3]/span")
+//    private WebElement date;
+//
+//    @FindBy(xpath = "//div[@data-csa-c-slot-id=\"seriesBulletWidget_feature_div\"]")
+//    private WebElement series;
+//
+//    @FindBy(xpath = "//span[@class=\"slot-price\"]")
+//    private static WebElement priceElement;
+
+
     public static List<String> bookDescription(int itemNumber){
 
         String path = "//div[@data-index=\"%d\" and @data-component-type=\"s-search-result\"]";
@@ -26,16 +46,47 @@ public class Book {
         for (String words : stringers){
             list.add(words);
         }
+//        List<String> authorList = new ArrayList<>();
+//        stringers = list.get(2).split("by ");
+//        for (String words : stringers){
+//            authorList.add("by "+words);
+//        }
+//        stringers = authorList.get(1).split("et al.");
+//        for (String words : stringers){
+//            authorList.add(words+"Trisha Gee et al.");
+//        }
+///////////////////////////////////////////////////////////////////////////////////////////
+
+        List<WebElement> authorElements = driver.findElements(By.xpath((String.format(path, itemNumber+1)+"//span[@class=\"a-size-base\"]")));
+        String author ="";
+        for (WebElement link:authorElements){
+           String lol = link.getText();
+           author = author+" "+lol;
+        }
+        author=author.replace(" ,", ",");
+        author=author.replace(" by", "by");
+
+
+        //////////////////////////////////////////////////////////////////////
         if (list.get(0).contains("Best Seller")){
-            System.out.println("It is a Best Seller");
+            String bs = "It is a Best Seller";
             String priceToRent = list.get(8)+"."+list.get(9);
             String priceToBuy = list.get(10)+"."+list.get(11);
-            bookDesc.add(list.get(1) + "\n" + list.get(2) + "\n" + priceToRent + "\n" + priceToBuy);
+            bookDesc.add(bs);
+            bookDesc.add(list.get(1));
+//            bookDesc.add(authorList.get(2));
+            bookDesc.add(author);
+            bookDesc.add(priceToRent);
+            bookDesc.add(priceToBuy);
         } else {
-            System.out.println("This isn't Best Seller");
+            String bs = "This isn't Best Seller";
             String priceToRent = list.get(7)+"."+list.get(8);
             String priceToBuy = list.get(9)+"."+list.get(10);
-            bookDesc.add(list.get(0)+"\n"+list.get(1)+list.get(2)+"\n"+priceToRent+"\n"+priceToBuy);
+            bookDesc.add(bs);
+            bookDesc.add(list.get(0));
+            bookDesc.add(author);
+            bookDesc.add(priceToRent);
+            bookDesc.add(priceToBuy);
         }
         return bookDesc;
     }
@@ -50,21 +101,24 @@ public class Book {
         WebElement priceElement = driver.findElement(By.xpath("//span[@class=\"slot-price\"]"));
         String[] stringers = priceElement.getText().split(" - ");
         List<String> price = new ArrayList<>();
-        price.add("\n"+stringers[0]+" to rent");
-        price.add("\n"+stringers[1]+" to buy");
+        price.add(stringers[0]+" to rent");
+        price.add(stringers[1]+" to buy");
 //        for (String words : stringers){
 //            price.add("\n"+words);
 //        }
 
-        String str = series.getText()+" | "+author.getText()+" | "+date.getText();
+//        String str = series.getText()+" | "+author.getText()+" | "+date.getText();
+        String str = author.getText();
 //        System.out.println("Part of: Head First (44 books) | by Kathy Sierra, Bert Bates, et al. | Jun 21, 2022");
         str=str.replace(" (Author)", "");
         str=str.replace("& 1 more", "et al.");
-        System.out.println(str);
         String bs;
         if (bestSeller.isDisplayed()){bs="It is a Best Seller";} else bs = "This isn't Best Seller";
         List<String> bookDesc = new ArrayList<>();
-        bookDesc.add(bs+"\n"+title.getText()+"\n"+str+price);
+        bookDesc.add(bs);
+        bookDesc.add(title.getText());
+        bookDesc.add(str);
+        bookDesc.addAll(price);
         return bookDesc;
     }
 }
